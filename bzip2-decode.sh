01fd752927e723fa553802aa93350dc6bbee489c
KuinDev <111401773+kuindev@users.noreply.github.com>
2022-09-18 16:16:01 +0700

    Create bzip2-decode.sh

---
+++ bzip2-decode.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+skip=23
+set -C
+umask=`umask`
+umask 77
+tmpfile=`mktemp gztmpXXXXX -p /tmp` || exit 1
+if /usr/bin/tail -n +$skip "$0" | /bin/bzip2 -cd >> $tmpfile; then
+  umask $umask
+  /bin/chmod 700 $tmpfile
+  prog="`echo $0 | /bin/sed 's|^.*/||'`"
+  if /bin/ln -T $tmpfile "/tmp/$prog" 2>/dev/null; then
+    trap '/bin/rm -f $tmpfile "/tmp/$prog"; exit $res' 0
+    (/bin/sleep 5; /bin/rm -f $tmpfile "/tmp/$prog") 2>/dev/null &
+    /tmp/"$prog" ${1+"$@"}; res=$?
+  else
+    trap '/bin/rm -f $tmpfile; exit $res' 0
+    (/bin/sleep 5; /bin/rm -f $tmpfile) 2>/dev/null &
+    $tmpfile ${1+"$@"}; res=$?
+  fi
+else
+  echo Cannot decompress $0; exit 1
+fi; exit $res
